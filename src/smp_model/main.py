c18c89fe3501ad51f393bb4720024c211828d5b7
Mikhailov.M <mikhailov.m@tr-support.ru>
2024-06-14 02:25:28 +0300

    добавлен расчет тяжести и расстояния

--- src/smp_model/main.py
+++ src/smp_model/main.py
@@ -5,7 +5,7 @@ from src.smp_model.input import ModelInput
 from src.smp_model.model import Model
 from src.smp_model.model_config import ModelConfig
 from src.smp_scenario.scenario_config import ScenarioConfig
-
+from src.smp_model.graph.length_velocity_calc import dump_velocity_length
 
 def run_model(
         input_folder_path: str,
@@ -17,6 +17,7 @@ def run_model(
         output_folder_path=output_folder_path,
         model_config=model_config,
     )
+    dump_velocity_length(input)
     model = Model(input)
     model.solve_model()
     model.output.create_output()
@@ -26,10 +27,10 @@ if __name__ == '__main__':
     input_folder_path = os.path.join('data', 'scenarios', 'base', 'input')
     output_folder_path = os.path.join('data', 'scenarios', 'base', 'output')
 
-    config = ScenarioConfig.create_from_json(os.path.join(input_folder_path, 'config.json'))
+    config = ScenarioConfig.create_from_json(r'D:\PycharmProjects\SMP\data\scenarios\base\input\config.json')
     model_config = config.get_model_config()
     run_model(
-        input_folder_path=input_folder_path,
+        input_folder_path=r'D:/PycharmProjects//SMP/data/scenarios/base/input/',
         output_folder_path=output_folder_path,
         model_config=model_config,
     )
--- src/smp_model/utils.py
+++ src/smp_model/utils.py
@@ -1,5 +1,6 @@
 from pyomo.core import Constraint
 import numpy as np
+from datetime import datetime
 
 np_true = np.bool_(True)
 
@@ -21,3 +22,42 @@ def constraints_from_dict(cons, model, prefix):
 	else:
 		result = Constraint(expr=cons)
 		setattr(model, prefix, result)
+
+
+def choose_week_for_calc(config_date, dates):
+	converted_month_dict = {
+		'Jan':"01",
+		'Feb':"02",
+		'Mar':"03",
+		'Apr':"04",
+		'May':"05",
+		'Jun':"07",
+		'Jul':"08",
+		'Aug':"09",
+		'Sep':"09",
+		'Oct':"10",
+		"Nov":"11",
+		'Dec':"12",
+	}
+	day_diff = {}
+	for date in dates:
+		try:
+			month = date.split('-')[1]
+			sheet_date = datetime.strptime(
+				date.replace(month, converted_month_dict.get(month,month)), "%d-%m-%Y"
+			)
+			diff = (sheet_date - config_date).days
+			if diff > 0:
+				continue
+			day_diff[date] = diff
+		except Exception:
+			continue
+	if day_diff:
+		week = max(day_diff, key=day_diff.get)
+	else:
+		if 'lat' in dates:
+			week = '03-Mar-2022'
+		else:
+			week = '03-03-2022'
+
+	return week
